fef911b1565935d129484443c7323325b4e65751
mbernth <michael@monovoce.dk>
2015-11-25 18:43:23 +0100

    Home page

--- Genesis-Faergecafeen-Theme/js/home.image.height.js
+++ Genesis-Faergecafeen-Theme/js/home.image.height.js
@@ -1,13 +1,13 @@
 // Image Section Height
 var windowHeight = $( window ).height();
 
-				$( '.image-section' ) .css({'height': windowHeight /'1.5' +'px'});
+				$( '.home-image-section' ) .css({'height': windowHeight /'1.5' +'px'});
 		
 				$( window ).resize(function(){
 	
 				var windowHeight = $( window ).height();
 			
-				$( '.image-section' ) .css({'height': windowHeight /'1.5' +'px'});
+				$( '.home-image-section' ) .css({'height': windowHeight /'1.5' +'px'});
 	
 });
 		
